051dcba58c2f21ffada2de518f440cba27e975f9
Alex Koval <301019a@gmail.com>
2023-01-23 13:24:53 +0200

    localStorage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,11 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
+const LS_KEY = 'savedContacts';
+
 export class App extends Component {
   state = {
-    contacts: [
+       contacts:[
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -16,6 +18,20 @@ export class App extends Component {
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const localData = localStorage.getItem(LS_KEY);
+    if (localData) {                                         //перевірка, що localStorage не пустий
+      this.setState({ contacts: JSON.parse(localData) });
+    } 
+    // else this.setState({ contacts: [] }); //перезатираємо localStorage
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleSubmit = stateOfContactForm => {
     const { contacts } = this.state;
